711946402b132223e546f91a521571a985e315c9
bheanne <70552452+bheanne@users.noreply.github.com>
2021-03-07 16:47:01 +0800

    Update Marq_Lab2.py

--- LAB REPORT 2/Marq_Lab2.py
+++ LAB REPORT 2/Marq_Lab2.py
@@ -56,7 +56,7 @@ plt.scatter(Vector5[0], Vector5[1], label='Vector5', c='lime')
 plt.scatter(Addition[0], Addition[1], label='Addition', c='aquamarine')
 plt.scatter(Subtraction[0], Subtraction[1], label='Subtraction', c='aqua')
 plt.scatter(Combi1[0], Combi1[1], label ='Add and Subtract', c='deepskyblue')
-plt.scatter(Divide[0], Divide[1], label='Division', c='blue')
+plt.scatter(Division[0], Division[1], label='Division', c='blue')
 plt.scatter(Multiplication[0], Multiplication[1], label='Multiplication', c='blue')
 plt.scatter(Combi2[0], Combi2[1], label='Multiply and Divide', c='darkviolet')
 plt.scatter(Square[0], Square[1], label='Square', c='fuchsia')
@@ -64,10 +64,10 @@ plt.scatter(Square_Root[0], Square_Root[1], label='Square Root', c='magenta')
 plt.scatter(Combi3[0], Combi3[1], label='Square and Square Root', c='deeppink')
 
 ##Resultant Vector
-plt.quiver(Vector2[0], Vector2[1], Vector1[0], Vector1[1], angles='xy', scale_units='xy', scale=1, color='mistyrose')
-plt.quiver(-20, -5, Vector4[0], Vector4[1], angles='xy', scale_units='xy', scale=1, color='lemonchiffon')
+plt.quiver(Vector1[0], Vector1[1], Vector2[0], Vector2[1], angles='xy', scale_units='xy', scale=1, color='mistyrose')
+plt.quiver(5, 21, Vector4[0], Vector4[1], angles='xy', scale_units='xy', scale=1, color='violet')
 Vector6 = Vector2 + Vector4
-plt.quiver(Vector2[0], Vector2[1], Vector6[0], Vector6[1], angles='xy', scale_units='xy', scale=1, color='skyblue')
+plt.quiver(Vector3[0], Vector3[1], Vector6[0], Vector6[1], angles='xy', scale_units='xy', scale=1, color='skyblue')
 
 R_mag = np.sqrt(np.sum(Vector1 ** 2 + Vector4 ** 2))  ##Euclidean Distance / Euclidean Norm
 rise = Vector6[1]
